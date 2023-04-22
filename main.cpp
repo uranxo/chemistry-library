20700e09d746e2f27e5a94a0a309b7aabd6cc6dc
uranxo <98714809+uranxo07@users.noreply.github.com>
2023-04-22 23:46:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-	string inputvar;
+	string inputvar; // you can change inputvar with any other var name
 	cout << "Insert the chemical formula" << endl;
 	cin >> inputvar;
 	cout << getmolarmass(inputvar) << endl;
