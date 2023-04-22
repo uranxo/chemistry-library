892a475c2ab88864b460945756857aa9f69d748e
uranxo <98714809+uranxo07@users.noreply.github.com>
2023-04-22 23:45:16 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 	string inputvar;
-	cout << "Inserisci la formula chimica" << endl;
+	cout << "Insert the chemical formula" << endl;
 	cin >> inputvar;
 	cout << getmolarmass(inputvar) << endl;
 	system("pause");
